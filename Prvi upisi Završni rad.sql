3a7be33cafb876eede14e919e425e21f344860b7
marimeros <merm2431@gmail.com>
2024-06-14 16:09:06 +0200

    Update Prvi upisi Završni rad.sql

--- Prvi upisi Završni rad.sql
+++ Prvi upisi Završni rad.sql
@@ -1,118 +1,34 @@
-USE [master]
-GO
 
-/****** Object:  Database [Vježba Završni rad]    Script Date: 14.6.2024. 16:01:35 ******/
-CREATE DATABASE [Vježba Završni rad]
- CONTAINMENT = NONE
- ON  PRIMARY 
-( NAME = N'Vježba Završni rad', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\Vježba Završni rad.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
- LOG ON 
-( NAME = N'Vježba Završni rad_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\Vježba Završni rad_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
- WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
-GO
 
-IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
-begin
-EXEC [Vježba Završni rad].[dbo].[sp_fulltext_database] @action = 'enable'
-end
-GO
+create table Clanova(
+sifra INT PRIMARY KEY,
+ime VARCHAR(50),
+prezime VARCHAR(50),
+clanskibroj VARCHAR(20),
+telefon VARCHAR(20));
 
-ALTER DATABASE [Vježba Završni rad] SET ANSI_NULL_DEFAULT OFF 
-GO
+create table Knjiga(
+sifra INT PRIMARY KEY,
+naziv VARCHAR(100),
+knjizevnavrsta VARCHAR(50));
 
-ALTER DATABASE [Vježba Završni rad] SET ANSI_NULLS OFF 
-GO
+create table Posudba(
+sifra INT PRIMARY KEY,
+sifraKnjiga INT,
+sifraClan INT,
+datumposudbe DATE,
+datumvracanja DATE,
+FOREIGN KEY (sifraKnjiga) REFERENCES Knjiga(sifra),
+FOREIGN KEY (sifraClan) REFERENCES Clanova(sifra));
 
-ALTER DATABASE [Vježba Završni rad] SET ANSI_PADDING OFF 
-GO
+insert into Clanova (sifra, ime, prezime, clanskibroj, telefon) values ('1', 'Ivo', 'Ivi�', '5531', '098242382');
+SELECT * FROM Clanova WHERE sifra =1;
 
-ALTER DATABASE [Vježba Završni rad] SET ANSI_WARNINGS OFF 
-GO
+insert into Knjiga (sifra, naziv, knjizevnavrsta) values ('1', 'Judita Marko Maruli�', 'ep');
 
-ALTER DATABASE [Vježba Završni rad] SET ARITHABORT OFF 
-GO
+insert into Posudba (sifra, sifraKnjiga, sifraClan, datumposudbe, datumvracanja) values ('1','1', '1', '01-01-2024', '02-02-2024');
 
-ALTER DATABASE [Vježba Završni rad] SET AUTO_CLOSE OFF 
-GO
+SELECT * FROM Knjiga WHERE sifra =1;
 
-ALTER DATABASE [Vježba Završni rad] SET AUTO_SHRINK OFF 
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET AUTO_UPDATE_STATISTICS ON 
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET CURSOR_CLOSE_ON_COMMIT OFF 
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET CURSOR_DEFAULT  GLOBAL 
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET CONCAT_NULL_YIELDS_NULL OFF 
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET NUMERIC_ROUNDABORT OFF 
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET QUOTED_IDENTIFIER OFF 
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET RECURSIVE_TRIGGERS OFF 
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET  DISABLE_BROKER 
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET DATE_CORRELATION_OPTIMIZATION OFF 
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET TRUSTWORTHY OFF 
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET ALLOW_SNAPSHOT_ISOLATION OFF 
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET PARAMETERIZATION SIMPLE 
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET READ_COMMITTED_SNAPSHOT OFF 
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET HONOR_BROKER_PRIORITY OFF 
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET RECOVERY FULL 
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET  MULTI_USER 
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET PAGE_VERIFY CHECKSUM  
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET DB_CHAINING OFF 
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET TARGET_RECOVERY_TIME = 60 SECONDS 
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET DELAYED_DURABILITY = DISABLED 
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET ACCELERATED_DATABASE_RECOVERY = OFF  
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET QUERY_STORE = ON
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
-GO
-
-ALTER DATABASE [Vježba Završni rad] SET  READ_WRITE 
-GO
+SELECT * FROM Posudba WHERE sifra =1;
 
