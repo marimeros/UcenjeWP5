5d3b39f8fcd6a751beaee36624a8fdfff0ef9ce2
marimeros <merm2431@gmail.com>
2024-06-14 14:14:40 +0200

    završni rad

---
+++ ZR.sql
@@ -0,0 +1,24 @@
+--use database UcenjeWP5;
+
+--create table Clanova(
+--sifra INT PRIMARY KEY,
+--ime VARCHAR(50),
+--prezime VARCHAR(50),
+--clanskibroj VARCHAR(20),
+--telefon VARCHAR(20));
+
+--create table Knjiga(
+--sifra INT PRIMARY KEY,
+--naziv VARCHAR(100),
+--knjizevnavrsta VARCHAR(50));
+
+--create table Posudba(
+--sifra INT PRIMARY KEY,
+--sifraKnjiga INT,
+--sifraClan INT,
+--datumposudbe DATE,
+--datumvracanja DATE,
+--FOREIGN KEY (sifraKnjiga) REFERENCES Knjiga(sifra),
+--FOREIGN KEY (sifraClan) REFERENCES Clanova(sifra));
+
+
