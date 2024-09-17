e18522c65f1a7be7db2a414b131a969da9b9ed7e
itomicbabinski <168195864+itomicbabinski@users.noreply.github.com>
2024-09-17 20:56:06 +0200

    Ispravak skripte

--- Završni rad Knjiznica2.sql
+++ Završni rad Knjiznica2.sql
@@ -11,28 +11,28 @@ prezime VARCHAR(50)COLLATE Croatian_CI_AS,
 clanskibroj VARCHAR(20),
 telefon VARCHAR(20));
 
-create table Knjiga(
+create table Knjige(
 sifra INT PRIMARY KEY identity(1,1),
 naziv VARCHAR(100) COLLATE Croatian_CI_AS,
 knjizevnavrsta VARCHAR(50)COLLATE Croatian_CI_AS
 );
 
-create table Posudba(
+create table Posudbe(
 sifra INT PRIMARY KEY identity(1,1),
 sifraKnjiga INT,
 sifraClan INT,
 datumposudbe DATE,
 datumvracanja DATE,
-FOREIGN KEY (sifraKnjiga) REFERENCES Knjiga(sifra),
+FOREIGN KEY (sifraKnjiga) REFERENCES Knjige(sifra),
 FOREIGN KEY (sifraClan) REFERENCES Clanova(sifra));
 
 insert into Clanova ( ime, prezime, clanskibroj, telefon) values ('Ivo', 'Ivić', '5531', '098242382');
 SELECT * FROM Clanova WHERE sifra =1;
 
-insert into Knjiga (naziv, knjizevnavrsta) values ( 'Judita Marko Marulić', 'ep');
+insert into Knjige (naziv, knjizevnavrsta) values ( 'Judita Marko Marulić', 'ep');
 
-insert into Posudba ( sifraKnjiga, sifraClan, datumposudbe, datumvracanja) values ('1', '1', '01-01-2024', '02-02-2024');
+insert into Posudbe ( sifraKnjiga, sifraClan, datumposudbe, datumvracanja) values ('1', '1', '01-01-2024', '02-02-2024');
 
-SELECT * FROM Knjiga WHERE sifra =1;
+SELECT * FROM Knjige WHERE sifra =1;
 
-SELECT * FROM Posudba WHERE sifra =1;
+SELECT * FROM Posudbe WHERE sifra =1;
