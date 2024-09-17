348454f7a230e73accfa8f0c9b9c711f52d20055
itomicbabinski <168195864+itomicbabinski@users.noreply.github.com>
2024-09-17 20:12:26 +0200

    Ispravak skripte

--- Završni rad Knjiznica2.sql
+++ Završni rad Knjiznica2.sql
@@ -5,20 +5,20 @@ go
 use Knjiznica2;
 go
 create table Clanova(
-sifra INT PRIMARY KEY,
+sifra INT PRIMARY KEY identity(1,1),
 ime VARCHAR(50) COLLATE Croatian_CI_AS,
 prezime VARCHAR(50)COLLATE Croatian_CI_AS,
 clanskibroj VARCHAR(20),
 telefon VARCHAR(20));
 
 create table Knjiga(
-sifra INT PRIMARY KEY,
+sifra INT PRIMARY KEY identity(1,1),
 naziv VARCHAR(100) COLLATE Croatian_CI_AS,
 knjizevnavrsta VARCHAR(50)COLLATE Croatian_CI_AS
 );
 
 create table Posudba(
-sifra INT PRIMARY KEY,
+sifra INT PRIMARY KEY identity(1,1),
 sifraKnjiga INT,
 sifraClan INT,
 datumposudbe DATE,
@@ -26,12 +26,12 @@ datumvracanja DATE,
 FOREIGN KEY (sifraKnjiga) REFERENCES Knjiga(sifra),
 FOREIGN KEY (sifraClan) REFERENCES Clanova(sifra));
 
-insert into Clanova (sifra, ime, prezime, clanskibroj, telefon) values ('1', 'Ivo', 'Ivić', '5531', '098242382');
+insert into Clanova ( ime, prezime, clanskibroj, telefon) values ('Ivo', 'Ivić', '5531', '098242382');
 SELECT * FROM Clanova WHERE sifra =1;
 
-insert into Knjiga (sifra, naziv, knjizevnavrsta) values ('1', 'Judita Marko Marulić', 'ep');
+insert into Knjiga (naziv, knjizevnavrsta) values ( 'Judita Marko Marulić', 'ep');
 
-insert into Posudba (sifra, sifraKnjiga, sifraClan, datumposudbe, datumvracanja) values ('1','1', '1', '01-01-2024', '02-02-2024');
+insert into Posudba ( sifraKnjiga, sifraClan, datumposudbe, datumvracanja) values ('1', '1', '01-01-2024', '02-02-2024');
 
 SELECT * FROM Knjiga WHERE sifra =1;
 
